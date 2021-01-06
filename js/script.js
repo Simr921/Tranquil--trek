33f8bf5d4c2c79073398c6bfdfb61f43553f2e65
Simr921 <simranpr345@gmail.com>
2021-01-06 21:16:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('button').addEventListener('click', function () {
+    document.querySelector('.login-click').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.login-click').style.display = 'none';
+});
+
+
